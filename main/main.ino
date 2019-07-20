4e8eb66d97f64b63a08f63daa97a59767323378f
Samuel <samjavaloyes96@gmail.com>
2019-07-20 03:20:42 +0200

    MEGA FIX/ TIMER WORKS

--- main/main.ino
+++ main/main.ino
@@ -1,17 +1,17 @@
 
 #include <LiquidCrystal.h> // Incluye la Libreria LiquidCrystal
 
+
 void setup() {
   Serial.begin(9600);
   init_blink();
   init_timer();
-  
-
-  pinMode(9, OUTPUT);
-
+  init_trigger();
+ 
 }
 
 void loop() {
-  m_timer();
+  //m_timer();
   m_blink(true,true);
+  m_trigger();
 }
