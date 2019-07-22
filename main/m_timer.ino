3287abb41bb9a32dc1e09f9f5b1ba196c9cca016
Samuel <samjavaloyes96@gmail.com>
2019-07-23 00:16:42 +0200

    Fix/ time intervals and 12pm problem Testes/ serial.print with intervals

--- main/m_timer.ino
+++ main/m_timer.ino
@@ -8,7 +8,13 @@ bool timer_getOnTime(int num_t)
   if (num_t < timer_num_intervalos)
   { 
     int timer_hora = rtc.getTime().hour;
-    if ((timer_hora > timer_intervalos[num_t][0] && timer_hora < timer_intervalos[num_t][1]))
+    if(timer_hora==0&&timer_intervalos[num_t][1]==24)
+    timer_hora=24;
+
+    Serial.print("---HOUR--->");
+    Serial.println(timer_hora);
+
+    if ((timer_hora >= timer_intervalos[num_t][0] && timer_hora <= timer_intervalos[num_t][1]))
       return true;
     else
      return false;
@@ -20,14 +26,21 @@ int timer_getGate(int i){
 }
 
 bool timer_gateIsOnTime(int g){
- 
+  
+  Serial.print("Check gate ");
+  Serial.println(g);
    for (unsigned i = 0; i < timer_num_intervalos; i++)
   {
  
+  Serial.print(" with interval  ");
+  Serial.println(i);
+
     if(timer_getOnTime(i)&&timer_intervalos[i][2]==g)
-    return true;
+    return true,Serial.println("--its on time--");
     
   }
+
+  Serial.println("--not in time--");
   return false;
 }
 
@@ -38,9 +51,9 @@ void m_timer()
 void init_timer()
 {
   //timer_num_intervalos tiene que ser igual al numero de intervalos
-  timer_intervalos[0][0] = 1,timer_intervalos[0][1] = 21,timer_intervalos[0][2] = 0 ;//luz
-  timer_intervalos[1][0] = 7,timer_intervalos[0][1] = 12,timer_intervalos[1][2] = 1 ;//oxi
-  timer_intervalos[2][0] = 18,timer_intervalos[0][1] = 22,timer_intervalos[2][2] = 1 ;//oxi
+  timer_intervalos[0][0] = 0,timer_intervalos[0][1] = 1,timer_intervalos[0][2] = 0 ;//luz
+  timer_intervalos[1][0] = 0,timer_intervalos[0][1] = 0,timer_intervalos[1][2] = 1 ;//oxi
+  timer_intervalos[2][0] = 7,timer_intervalos[0][1] = 12,timer_intervalos[2][2] = 1 ;//oxi
 
   rtc.begin();
 }
